104f28db9cc84a5821fba997ea09392a81c752ab
virag-ky <vkormoczy@gmail.com>
2025-03-22 16:22:37 +0200

    Add expo-image library and add Image component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { Image } from "expo-image";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
     </View>
   );
 }
@@ -19,12 +21,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
