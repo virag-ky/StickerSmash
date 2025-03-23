b2a1353cf64c81b8e07470cde43976b557c103d9
virag-ky <vkormoczy@gmail.com>
2025-03-23 17:38:18 +0200

    Display option buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -29,6 +29,14 @@ export default function Index() {
     }
   };
 
+  const onReset = () => {
+    setShowAppOPtions(false);
+  };
+
+  const onAddSticker = () => {};
+
+  const onSaveImageAsync = () => {};
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
@@ -38,7 +46,17 @@ export default function Index() {
         />
       </View>
       {showAppOptions ? (
-        <View />
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="Save"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
       ) : (
         <View style={styles.footerContainer}>
           <Button
@@ -69,5 +87,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
