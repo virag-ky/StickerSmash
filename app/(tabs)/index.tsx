6b1132e564463f75e8d595840b545280b56be09c
virag-ky <vkormoczy@gmail.com>
2025-03-23 15:04:06 +0200

    Add onPress prop to the primary button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -26,7 +26,11 @@ export default function Index() {
         <ImageViewer imgSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
     </View>
