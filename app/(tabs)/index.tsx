0aa4ef8e75864f9a1540c2e9f614f891e6a6bcdb
virag-ky <vkormoczy@gmail.com>
2025-03-25 21:01:25 +0200

    Display selected emoji

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,6 +7,7 @@ import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
 import EmojiPicker from "@/components/EmojiPicker";
 import EmojiList from "@/components/EmojiList";
+import EmojiSticker from "@/components/EmojiSticker";
 import { type ImageSource } from "expo-image";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
@@ -57,6 +58,9 @@ export default function Index() {
           imgSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
+        {pickedEmoji && (
+          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+        )}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
